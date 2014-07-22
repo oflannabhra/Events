0fb73aeb7b839bafc3a8d1be28e6641aa1cf2bc1
oflannabhra <oflannabhra@gmail.com>
2014-07-21 21:40:03 -0400

    user table creation, views

---
+++ app/views/user/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts/default')
+
+@section('Content')
+
+	<div>
+		<div class="container">
+		<h2>Register for Events</h2>
+			{{ Form::open( ['route' => 'user.store'] ) }}
+				{{ Form::text('username', null, array('placeholder' => 'Username', 'class' => 'form-control', 'autofocus')) }}
+				{{ Form::text('password', null, array('placeholder' => 'Password', 'class' => 'form-control')) }}
+				{{ Form::submit() }}
+			{{ Form::close() }}
+		</div>
+	</div>
+@stop
